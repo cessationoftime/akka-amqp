7e8deee1dc6d1b284f330bbd765e797e1aa514e9
Jeff Van Vranken <jeff@psyonik.com>
2014-08-02 15:16:43 -0400

    Updated to Akka 2.3.3.

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -6,11 +6,11 @@ object dependencies {
   def AmqpClient = "com.rabbitmq" % "amqp-client" % "3.1.4"   													  // ApacheV2
 
  
-  def AkkaAgent = "com.typesafe.akka" % "akka-agent_2.10" % "2.2.3"
+  def AkkaAgent = "com.typesafe.akka" % "akka-agent_2.10" % "2.3.3"
 
   def Specs2      = "org.specs2"                 % "specs2_2.10"              % "1.13"        % "test"  // MIT
   def JUnit = "junit" % "junit" % "4.7" % "test"   																 // Common Public License 1.0
-  def AkkaTestKit = "com.typesafe.akka" % "akka-testkit_2.10" % "2.2.3" % "test"
+  def AkkaTestKit = "com.typesafe.akka" % "akka-testkit_2.10" % "2.3.3" % "test"
   //def ActorTests = "com.typesafe.akka" % "akka-actor-tests_2.10.1" % "2.1-M2" % "test"
   def Mockito = "org.mockito" % "mockito-all" % "1.9.0" % "test"     											// MIT
 }
